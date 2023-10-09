1a26191ae8bf94b5c2a64ee918b759599f55fb32
Yuto Tamura <cayt1224@gmail.com>
2023-10-09 14:59:08 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,23 @@
+var variableNumber = 123;
+console.log(variableNumber);
+
+var variableString = "practice";
+console.log(variableString);
+
+var variableBoolean = true;
+console.log(variableBoolean);
+
+var addNumber = 10 + 20;
+console.log(addNumber);
+
+var subNumber = 20 - 10;
+console.log(subNumber);
+
+var timesNumber = 50 * 2;
+console.log(timesNumber);
+
+var divNumber = 20 / 2;
+console.log(divNumber);
+
+var conbinationNumber = "12" + "34";
+console.log(conbinationNumber);
